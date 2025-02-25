136462ee93536f8987a019061e31ef64874bc55e
DARK-H4CKER01 <dark.s.hacking007@gmail.com>
2025-02-26 02:52:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -220,6 +220,7 @@ termux() {
     pkg install jq -y >/dev/null 2>&1
     pkg install figlet -y >/dev/null 2>&1
     pkg install termux-api -y >/dev/null 2>&1
+    pkg install lsd -y >/dev/null 2>&1
     termux-reload-settings
 }
 
